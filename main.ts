305c9d46b9d86d24011936e71c52172656961a15
AyushR-Ranjan <122555382+AyushR-Ranjan@users.noreply.github.com>
2023-01-31 11:46:39 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 function ScoreBoard () {
+    OLED.clear()
     OLED.writeStringNewLine("PlayerA" + "=" + PA)
     OLED.newLine()
     OLED.writeStringNewLine("PlayerB" + "=" + PB)
@@ -14,7 +15,6 @@ input.onButtonPressed(Button.A, function () {
     ScoreBoard()
     basic.pause(2000)
     basic.clearScreen()
-    OLED.clear()
 })
 input.onButtonPressed(Button.AB, function () {
     basic.showString("T")
@@ -23,7 +23,6 @@ input.onButtonPressed(Button.AB, function () {
     ScoreBoard()
     basic.pause(2000)
     basic.clearScreen()
-    OLED.clear()
 })
 input.onButtonPressed(Button.B, function () {
     basic.showString("B")
@@ -32,24 +31,22 @@ input.onButtonPressed(Button.B, function () {
     ScoreBoard()
     basic.pause(2000)
     basic.clearScreen()
-    OLED.clear()
 })
 input.onGesture(Gesture.Shake, function () {
+    reset()
+})
+function reset () {
+    OLED.init(128, 64)
     PA = 0
     PB = 0
     Ties = 0
     Rounds = 0
     OLED.writeStringNewLine("Shall we play a game?")
-})
+    basic.pause(2000)
+    OLED.clear()
+}
 let Rounds = 0
 let Ties = 0
 let PB = 0
 let PA = 0
-OLED.init(128, 64)
-PA = 0
-PB = 0
-Ties = 0
-Rounds = 0
-OLED.writeStringNewLine("Shall we play a game?")
-basic.pause(2000)
-OLED.clear()
+reset()
